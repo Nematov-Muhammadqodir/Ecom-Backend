b85d4bd29303b9f68ac7081d50b027b05f71d52e
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-01-21 23:06:51 +0900

    addImage

--- src/main/java/com/kevin/SpringEcom/controller/ProductController.java
+++ src/main/java/com/kevin/SpringEcom/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.kevin.SpringEcom.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,8 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kevin.SpringEcom.model.Product;
 import com.kevin.SpringEcom.service.ProductService;
@@ -35,6 +39,17 @@ public class ProductController {
         } else {
 
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        Product savedProduct;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/kevin/SpringEcom/service/ProductService.java
+++ src/main/java/com/kevin/SpringEcom/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.kevin.SpringEcom.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kevin.SpringEcom.model.Product;
 import com.kevin.SpringEcom.repo.ProductRepo;
@@ -21,5 +23,13 @@ public class ProductService {
     public Product getProductById(int productId) {
         Product product = productRepo.findById(productId).get();
         return product;
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
     }
 }
